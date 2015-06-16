f2ecbe71443636f305ac1968f2d8bbcaba145513
Denis Kilchichakov <denis@kilchichakov.com>
2015-06-16 21:37:09 +0300

    A bit of work on PDDict. Bugfixes on init_data, tests for new public method add_definition

--- pd_dict.rb
+++ pd_dict.rb
@@ -12,26 +12,44 @@ class PDDict
   #init_data - either first, undefined word to start, or filename of storage  
   def initialize(init_data)
     raise ArgumentError unless (init_data.is_a? String)
-    if valid?(init_data)
-      #proper word, init dictionary with it
-      @data = {process_word(init_data) => nil}
-    else
-      #may be filename of storage
+    @data = {}
+    if valid?(init_data) #proper word, init dictionary with it
+      @data[process_word(init_data)] = nil
+    else #may be filename of storage
       @storage = init_data
       load_data()
+    end
+  end
+  
+  #TODO give first undefined word from @data
+  def request_definition()
+  end
+  
+  #Adds definition for existing word. Also adds new words to define
+  def add_definition(word, definition)
+    if (@data.has_key?(word) and @data[word] == nil and definition.is_a? String)
+      @data[word] = definition
+      definition.split.each do | w |
+          new_word = process_word(w)
+          if valid?(new_word) and !@data.has_key?(new_word)
+            @data[new_word] = nil
+          end
+      end
+    else
+      raise ArgumentError
     end
   end
   
   private
   
   def process_word(word)
-    #more processing may be added
-    return word.downcase
+    #leave only latin letters(used to strip words from definition) and downcase
+    return word.match(/[a-zA-Z]+/).to_s.downcase
   end
   
   def valid?(s)
     #is it just plain latin?
-    s.match(/[^a-zA-Z]/).to_s == ""
+    s.match(/[^a-zA-Z]/).to_s == "" and s.length > 0
   end
   
   def load_data() 
--- tc_pd_dict.rb
+++ tc_pd_dict.rb
@@ -14,6 +14,7 @@ class TestPDDict < Test::Unit::TestCase
   end
   
   def test_init_raise
+    assert_raise(ArgumentError) { (PDDict.new("")) }
     assert_raise(ArgumentError) { (PDDict.new("nonexistent.json")) }
     assert_raise(ArgumentError) { (PDDict.new(".,:-")) }
     assert_raise(ArgumentError) { (PDDict.new("Кириллица")) }
@@ -21,4 +22,46 @@ class TestPDDict < Test::Unit::TestCase
     assert_raise(ArgumentError) { (PDDict.new(125)) }
   end
   
+  def test_add_def
+    test_obj = PDDict.new("test")
+    test_obj.add_definition("test", "!,some' ,45; definition")
+    assert_equal({"test" => "!,some' ,45; definition", 
+                  "some" => nil, "definition" => nil}, 
+                 test_obj.data)
+    
+    test_obj.add_definition("some", "definition!!123 test-'`;")
+    assert_equal({"test" => "!,some' ,45; definition", 
+                  "some" => "definition!!123 test-'`;", "definition" => nil}, 
+                 test_obj.data)
+    
+    test_obj.add_definition("definition", "1999")
+    assert_equal({"test" => "!,some' ,45; definition", 
+                  "some" => "definition!!123 test-'`;", "definition" => "1999"}, 
+                 test_obj.data)
+    
+    #check tabs and newlines in definition
+    test_obj = PDDict.new("test")
+    test_obj.add_definition("test", "test \tsome \r\ndefinition")
+    assert_equal({"test" => "test \tsome \r\ndefinition", 
+                  "some" => nil, "definition" => nil}, 
+                 test_obj.data)
+  end
+  
+  def test_add_def_raise
+    assert_raise(ArgumentError) {
+      test_obj = PDDict.new("test")      
+      test_obj.add_definition("some", "test")
+    }
+      
+    assert_raise(ArgumentError) {
+      test_obj = PDDict.new("test")
+      test_obj.add_definition("some", 42)
+    }
+    
+    assert_raise(ArgumentError) {
+      test_obj = PDDict.new("test")
+      test_obj.add_definition("some", "explanation")
+      test_obj.add_definition("some", "another one")
+    }
+  end
 end
