7b504876dfe04be18abd348045a557a84aad50d6
Denis Kilchichakov <denis@kilchichakov.com>
2015-07-08 22:42:42 +0300

    A bit of refactoring in PDDict. Implemented request word definition, save and load from file (in JSON). All new features covered with tests. Adjusted formatting in both files (mostly spaces inside classes). PDDict class seems to be complete now.

--- pd_dict.rb
+++ pd_dict.rb
@@ -1,6 +1,9 @@
 #!/usr/bin/env ruby
 #encoding: utf-8
 
+require 'logger'
+require 'json'
+
 #"Please Define" Dictionary - class that works with dictionary data 
 class PDDict
   
@@ -11,32 +14,45 @@ class PDDict
   #file, which keeps dictionary data between runs (JSON-formatted)
   attr_reader :storage
 
+  
   #init_data - either first, undefined word to start, or filename of storage  
-  def initialize(init_data)
+  def initialize(init_data, verbose = false)
     raise ArgumentError unless (init_data.is_a? String)
     @data = {}
+    #auxiliary array for making undefined words search among @data more efficient
+    @undef_words = []
     @ref_stat = {}
+    @verbose = verbose
+    @logger = Logger.new(STDOUT) if @verbose
     if valid?(init_data) #proper word, init dictionary with it
-      @data[process_word(init_data)] = nil
+      log "inited with word: '#{init_data}'"
+      add_word(process_word(init_data))
     else #may be filename of storage
+      log "init from file: #{init_data}"
       @storage = init_data
       load_data()
     end
   end
+
   
-  #TODO give first undefined word from @data
+  #if it returns nil, then dictionary is completed
   def request_definition()
+    result = @undef_words[0]
+    log "define: '#{result}'"
+    return result    
   end
+
   
   #Adds definition for existing word. Also adds new words to define
   def add_definition(word, definition)
     if (@data.has_key?(word) and @data[word].nil? and definition.is_a? String)
+      log "'#{word}'' defined as: '#{definition}'"
       @data[word] = definition
+      @undef_words.delete(word)
       new_words = definition.split.map { |w| process_word(w) }.uniq
       new_words.each do |new_word|
           if valid?(new_word) 
-            @data[new_word] = nil unless @data.has_key?(new_word)
-
+            add_word(new_word) unless @data.has_key?(new_word)
             #word repeating in definition doesn't count as reference
             unless (word == new_word) 
               if (@ref_stat[new_word].nil?) 
@@ -51,26 +67,53 @@ class PDDict
       raise ArgumentError
     end
   end
+
+  
+  def save(storage = nil)
+    unless storage.nil?
+      raise ArgumentError unless storage.is_a? String
+      @storage = storage 
+    end
+    log "asked to save data in '#{@storage}'"
+    save_data()
+  end
+
   
   private
+  
+  
+  def add_word(word)
+    log "'#{word}' added"
+    @data[word] = nil
+    @undef_words << word
+  end
+
   
   def process_word(word)
     #leave only latin letters(used to strip words from definition) and downcase
     return word.match(/[a-zA-Z]+/).to_s.downcase
   end
+
   
   def valid?(s)
     #is it just plain latin?
     s.match(/[^a-zA-Z]/).to_s == "" and s.length > 0
   end
+
   
-  def load_data() 
-    #TOTHINK: is it correct to raise ArgumentErr upon instance vars?
-    raise ArgumentError unless File.exist?(@storage)
+  def log(s)
+    @logger.info(s) if @verbose
   end
   
+  
+  def load_data() 
+    raise ArgumentError unless File.exist?(@storage)
+    @data, @undef_words, @ref_stat = JSON.parse(File.read(@storage))
+  end
+
+  
   def save_data()
-    #dump @data to @storage
+    File.open(@storage, 'w') { |f| f.puts [@data, @undef_words, @ref_stat].to_json }
   end
   
 end
--- tc_pd_dict.rb
+++ tc_pd_dict.rb
@@ -5,13 +5,12 @@ require_relative "pd_dict"
 require "test/unit"
 
 class TestPDDict < Test::Unit::TestCase
-  
+
   def test_init
     assert_equal({"test" => nil}, PDDict.new("TEST").data)
     assert_equal({"test" => nil}, PDDict.new("test").data)
-    File.open("st.json", "w+") {}
-    assert_equal("st.json", PDDict.new("st.json").storage)
   end
+
   
   def test_init_raise
     assert_raise(ArgumentError) { (PDDict.new("")) }
@@ -21,6 +20,7 @@ class TestPDDict < Test::Unit::TestCase
     assert_raise(ArgumentError) { (PDDict.new(nil)) }
     assert_raise(ArgumentError) { (PDDict.new(125)) }
   end
+
   
   def test_add_def
     test_obj = PDDict.new("test")
@@ -52,7 +52,6 @@ class TestPDDict < Test::Unit::TestCase
     assert_equal({"test" => 1, "some" => 2, "definition" => 2},
                   test_obj.ref_stat)
     
-    
     #check tabs and newlines in definition
     test_obj = PDDict.new("test")
     test_obj.add_definition("test", "test \tsome \r\ndefinition")
@@ -60,6 +59,58 @@ class TestPDDict < Test::Unit::TestCase
                   "some" => nil, "definition" => nil}, 
                  test_obj.data)
   end
+
+  
+  def test_req_def 
+    test_obj = PDDict.new("test")
+    assert_equal("test", test_obj.request_definition)
+    
+    test_obj = PDDict.new("test")
+    word = test_obj.request_definition
+    test_obj.add_definition(word, "some test some definition stuff")
+    assert_equal("some", test_obj.request_definition)
+    test_obj.add_definition("some", "test")
+    assert_equal("definition", test_obj.request_definition)
+    test_obj.add_definition("definition", "test")
+    assert_equal("stuff", test_obj.request_definition)
+    test_obj.add_definition("stuff", "test")
+    assert_equal(nil, test_obj.request_definition)    
+  end
+
+  
+  def test_save_load
+    p = PDDict.new("test")
+    w = p.request_definition
+    p.add_definition(w, "some test definition stuff")
+    p.save('dump.json')
+    
+    p2 = PDDict.new('dump.json')
+
+    assert_equal(p.data, p2.data)
+    assert_equal(p.ref_stat, p2.ref_stat)
+    assert_equal(p.storage, p2.storage)
+    
+    File.delete('dump.json')
+  end
+
+  
+  def test_save_raise
+    assert_raise(TypeError) {
+      p = PDDict.new("test")
+      p.save
+    }
+    
+    assert_raise(ArgumentError) {
+      p = PDDict.new("test")
+      p.save 42
+    }
+
+    assert_raise(Errno::EACCES) {
+      p = PDDict.new("test")
+      p.save '//\\'
+    }
+  end
+  
   
   def test_add_def_raise
     assert_raise(ArgumentError) {
@@ -78,4 +129,5 @@ class TestPDDict < Test::Unit::TestCase
       test_obj.add_definition("some", "another one")
     }
   end
+  
 end
